18b1bf51902577941d280bb560a8e7af014e5f1b
Shreya <shreyadusane95@gmail.com>
2022-09-09 22:02:05 +0530

    area of circle, square, rectangle

---
+++ circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    float radius,area;
+    printf("Enter the radius of cicle:");
+    scanf("%d",&radius);
+    area=3.14*radius*radius;
+    printf("Area of circle is: %f",area);
+
+    return(0);
+
+
+}
---
+++ rectangle.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ int main(){
+  float length,breadth,area;
+
+ printf("Enter the length of the rectangle: ");
+ scanf("%f",&length);
+
+ printf("Enter breadth of the rectangle: ");
+  scanf("%f",&breadth);
+
+  area=(length*breadth);
+
+  printf("Area of Rectangle : %f\n",area);
+
+  return 0;
+
+
+
+ }
