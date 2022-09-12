73500c06345464490886883d77f61bedc1933dd7
Shreya <shreyadusane95@gmail.com>
2022-09-12 20:53:17 +0530

    perimeter of square

---
+++ peri of sqaure.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   float side,perimeter;
+   printf("enter side of square:");
+   scanf("%f",&side);
+
+   perimeter=4*side;
+   printf("POS:%f\n",perimeter);
+
+   return 0;
+
+
+}
